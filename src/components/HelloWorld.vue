594719993c916b1a5d42873039cebdbb2c3cf967
Milos Medic <milos.medic@viewpnt.com>
2020-01-25 13:55:45 +0100

    vue init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="hello">
+        test
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'HelloWorld',
+    }
+</script>
+
+<style scoped>
+
+</style>
